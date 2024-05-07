3f62fddd90bad6f27f7c6d3d3a943cca132346e7
Steve Phelps <79sphelps@gmail.com>
2024-05-07 11:59:33 -0700

    removing vercel.json

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-
 const app = express();
 
 if (process.env.NODE_ENV !== "dev") {
